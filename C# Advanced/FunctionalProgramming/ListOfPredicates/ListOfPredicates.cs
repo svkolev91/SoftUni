4a1d9dec566fc60c51020045000b41c1c643733b
svkolev91 <svkolev69@gmail.com>
2022-06-10 10:14:30 +0300

    More functional programming exercises.

---
+++ C# Advanced/FunctionalProgramming/ListOfPredicates/ListOfPredicates.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class ListOfPredicates
+{
+
+    //Find all numbers in the range 1…N that are divisible by the numbers of a given sequence.
+    //On the first line, you will be given an integer N – which is the end of the range.
+    //On the second line, you will be given a sequence of integers which are the dividers.Use predicates/functions.
+
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        List<int> dividers = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+
+
+        List<int> results = new List<int>();
+
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (CheckDivisible(dividers, i))
+            {
+                results.Add(i);
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", results));
+    }
+
+    static bool CheckDivisible(List<int> dividers, int i)
+    {
+        Predicate<int> isDivisible = num => i % num == 0;
+        foreach (var divider in dividers)
+        {
+            if (!isDivisible(divider))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ C# Advanced/FunctionalProgramming/PredicateForNames/PredicateForNames.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class PredicateForNames
+{
+
+    //Write a program that filters a list of names according to their length.
+    //On the first line, you will be given an integer n, representing a name's length.
+    //On the second line, you will be given some names as strings separated by space.
+    //Write a function that prints only the names whose length is less than or equal to n.
+
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        List<string> names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+        Predicate<string> isNameValid = name => name.Length <= n;
+        Action<List<string>> filteredNames = list => Console.WriteLine(string.Join("\r\n", list.FindAll(isNameValid)));
+
+        filteredNames(names);
+    }
+
+}
