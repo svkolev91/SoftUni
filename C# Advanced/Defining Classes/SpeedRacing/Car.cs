f8795efc0a2678ec13a6305865dbdb09e74aea7a
svkolev91 <svkolev69@gmail.com>
2022-06-13 21:07:06 +0300

    Classes and class methods.

---
+++ C# Advanced/Defining Classes/SpeedRacing/Car.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKilometer;
+        private double travelledDistance;
+
+        public Car()
+        {
+            travelledDistance = 0;
+        }
+
+        public Car (string model, double fuelAmount, double fuelConsumptionPerKilometer)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+        }
+
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public double FuelAmount
+        {
+            get { return fuelAmount; }
+            set { fuelAmount = value; }
+        }
+
+        public double FuelConsumptionPerKilometer
+        {
+            get { return fuelConsumptionPerKilometer; }
+            set { fuelConsumptionPerKilometer = value; }
+        }
+
+        public double TravelledDistance
+        {
+            get { return travelledDistance; }
+            set 
+            {
+                travelledDistance = value;
+            }
+        }
+
+        public void CarMove(double km)
+        {
+            
+            double litersNeeded = this.fuelConsumptionPerKilometer * km;
+            if (litersNeeded <= this.fuelAmount)
+            {
+                this.fuelAmount -= litersNeeded;
+                this.travelledDistance += km;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes/SpeedRacing/SpeedRacing.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpeedRacing
+{
+    internal class SpeedRacing
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] carDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string carModel = carDetails[0];
+                foreach (var car in cars)
+                {
+                    if (car.Model == carModel)
+                    {
+                        continue;
+                    }
+                }
+
+                double carFuel = double.Parse(carDetails[1]);
+                double carConsumption = double.Parse(carDetails[2]);
+
+                Car currentCar = new Car(carModel, carFuel, carConsumption);
+
+                
+
+                cars.Add(currentCar);
+            }
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string carModel = input.Split(" ")[1];
+                double kmToMove = double.Parse(input.Split(" ")[2]);
+
+                cars.First(car => car.Model == carModel).CarMove(kmToMove);
+
+                input = Console.ReadLine();
+            }
+
+            PrintCars(cars);
+        }
+
+        private static void PrintCars(List<Car> cars)
+        {
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
+            }
+        }
+    }
+}
