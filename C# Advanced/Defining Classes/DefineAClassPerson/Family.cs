bfdca7151b65f61cf6a6f06fcdab182c6a4e5e7d
svkolev91 <svkolev69@gmail.com>
2022-06-13 16:40:14 +0300

    Defining classes

---
+++ C# Advanced/Defining Classes/DefineAClassPerson/Family.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> familyMembers;
+
+        public Family()
+        {
+            this.familyMembers = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            this.familyMembers.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            int oldestMember = this.familyMembers.Max(member => member.Age);
+            return this.familyMembers.First(member => member.Age == oldestMember);
+        }
+
+        public void PrintMembers()
+        {
+            foreach (var person in familyMembers.OrderBy(member => member.Name))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes/DefineAClassPerson/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family myFamily = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                if (age > 30)
+                {
+                    Person member = new Person(name, age);
+                    myFamily.AddMember(member);
+                }
+            }
+
+            myFamily.PrintMembers();
+
+        }
+    }
+}
