6ef92783549d4671dc4ce85fd68222ab3d1202d2
svkolev91 <svkolev69@gmail.com>
2022-06-23 14:44:44 +0300

    Exam prep tasks.

---
+++ C# Advanced/AdvancedModuleExamPrep/Blacksmith/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] input1 = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+        int[] input2 = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        Queue<int> steel = new Queue<int>(input1);
+        Stack<int> carbon = new Stack<int>(input2);
+        Dictionary<string, int> swords = new Dictionary<string, int>();
+        int craftedSwordsCount = 0;
+
+        while (steel.Count > 0 && carbon.Count > 0)
+        {
+
+            int currentSteel = steel.Dequeue();
+            int currentCarbon = carbon.Pop();
+            int currentMixture = currentSteel + currentCarbon;
+            string sword = string.Empty;
+
+            if(string.IsNullOrEmpty(CheckIfSwordCanBeCrafter(currentMixture)))
+            {
+                carbon.Push(currentCarbon + 5);
+            }
+            else
+            {
+                sword = CheckIfSwordCanBeCrafter(currentMixture);
+                if (!swords.ContainsKey(sword))
+                {
+                    swords.Add(sword, 0);
+                }
+                swords[sword]++;
+                craftedSwordsCount++;
+            }
+        }
+
+        if (craftedSwordsCount == 0)
+        {
+            Console.WriteLine("You did not have enough resources to forge a sword.");
+        }
+        else
+        {
+            Console.WriteLine($"You have forged {craftedSwordsCount} swords.");
+        }
+
+        PrintSteelAndCarbonLeft(steel,carbon);
+
+        foreach (var sword in swords.OrderBy(s => s.Key))
+        {
+            Console.WriteLine($"{sword.Key}: {sword.Value}");
+        }
+    }
+
+
+    private static string CheckIfSwordCanBeCrafter(int currentMixture)
+    {
+        string sword = string.Empty;
+        if (currentMixture == 70)
+        {
+            sword = "Gladius";
+        }
+        else if (currentMixture == 80)
+        {
+            sword = "Shamshir";
+        }
+        else if (currentMixture == 90)
+        {
+            sword = "Katana";
+        }
+        else if (currentMixture == 110)
+        {
+            sword = "Sabre";
+        }
+        else if (currentMixture == 150)
+        {
+            sword = "Broadsword";
+        }
+
+        return sword;
+        
+    }
+
+    private static void PrintSteelAndCarbonLeft(Queue<int> steel, Stack<int> carbon)
+    {
+        Console.Write("Steel left: ");
+        if (steel.Count > 0)
+        {
+            Console.WriteLine(string.Join(", ", steel));
+        }
+        else
+        {
+            Console.WriteLine("none");
+        }
+
+        Console.Write("Carbon left: ");
+        if (carbon.Count > 0)
+        {
+            Console.WriteLine(string.Join(", ", carbon));
+        }
+        else
+        {
+            Console.WriteLine("none");
+        }
+    }
+}
---
+++ C# Advanced/AdvancedModuleExamPrep/TruffleHunter/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        char[,] forest = new char[n, n];
+
+        FillMatrix(forest);
+        Dictionary<char, int> trufflesCollected = new Dictionary<char, int>();
+        int eatenByBoar = 0;
+
+        while (true)
+        {
+            string command = Console.ReadLine();
+            if (command == "Stop the hunt")
+            {
+                break;
+            }
+
+            string[] cmdArgs = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            int currRow = int.Parse(cmdArgs[1]);
+            int currCol = int.Parse(cmdArgs[2]);
+
+            if (cmdArgs[0] == "Collect")
+            {
+                if (!CheckIfValid(forest, currRow, currCol))
+                {
+                    continue;
+                }
+
+                if (forest[currRow,currCol] != '-') 
+                {
+                    char trufle = forest[currRow, currCol];
+                    if (!trufflesCollected.ContainsKey(trufle))
+                    {
+                        trufflesCollected.Add(trufle, 0);
+                    }
+                    trufflesCollected[trufle]++;
+                    forest[currRow, currCol] = '-';
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            else if (cmdArgs[0] == "Wild_Boar")
+            {
+                string direction = cmdArgs[3];
+
+                if (forest[currRow, currCol] != '-')
+                {
+                    eatenByBoar++;
+                    forest[currRow, currCol] = '-';
+                }
+
+                eatenByBoar = MoveBoar(forest,currRow,currCol,eatenByBoar,direction);
+            }
+
+        }
+
+        PrintResults(trufflesCollected, eatenByBoar,forest);
+    }
+
+    private static void PrintResults(Dictionary<char, int> trufflesCollected, int eatenByBoar, char[,] forest)
+    {
+        Console.WriteLine($"Peter manages to harvest {trufflesCollected['B']} black, {trufflesCollected['S']} summer, and {trufflesCollected['W']} white truffles.");
+        Console.WriteLine($"The wild boar has eaten {eatenByBoar} truffles.");
+
+        for (int row = 0; row < forest.GetLength(0); row++)
+        {
+            for (int col = 0; col < forest.GetLength(1); col++)
+            {
+                Console.Write($"{forest[row, col]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    private static int MoveBoar(char[,] forest, int currRow, int currCol, int eatenByBoar, string direction)
+    {
+        if (direction == "up")
+        {
+            for (int row = currRow - 2; row >= 0; row-=2)
+            {
+                if (forest[row, currCol] != '-')
+                {
+                    eatenByBoar++;
+                    forest[row, currCol] = '-';
+                }
+            }
+        }
+        else if (direction == "down")
+        {
+            for (int row = currRow + 2; row < forest.GetLength(0); row+=2)
+            {
+                if (forest[row, currCol] != '-')
+                {
+                    eatenByBoar++;
+                    forest[row, currCol] = '-';
+                }
+            }
+        }
+        else if (direction == "left")
+        {
+            for (int col = currCol - 2; col >= 0; col-= 2)
+            {
+                if (forest[currRow, col] != '-')
+                {
+                    eatenByBoar++;
+                    forest[currRow, col] = '-';
+                }
+            }
+        }
+        else if (direction == "right")
+        {
+            for (int col = currCol + 2; col < forest.GetLength(1); col += 2)
+            {
+                if (forest[currRow, col] != '-')
+                {
+                    eatenByBoar++;
+                    forest[currRow, col] = '-';
+                }
+            }
+        }
+
+        return eatenByBoar;
+    }
+
+    private static bool CheckIfValid(char[,] forest, int currRow, int currCol)
+    {
+        if (currRow > forest.GetLength(0) || currRow < 0 ||
+            currCol > forest.GetLength(1) || currCol < 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static void FillMatrix(char[,] forest)
+    {
+        for (int row = 0; row < forest.GetLength(0); row++)
+        {
+            char[] rowData = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(char.Parse)
+                .ToArray();
+            for (int col = 0; col < forest.GetLength(1); col++)
+            {
+                forest[row, col] = rowData[col];
+            }
+        }
+    }
+}
