65e9b141e48f95e90d349b9340ec7ab027273c5e
svkolev91 <svkolev69@gmail.com>
2022-06-23 12:19:08 +0300

    Exam prep exercises.

---
+++ C# Advanced/AdvancedModuleExamPrep/Drones/Airfield.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Drones
+{
+    public class Airfield
+    {
+        public List<Drone> Drones { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public double LandingStrip { get; set; }
+
+        public Airfield(string name, int capacity, double landingStrip)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.LandingStrip = landingStrip;
+            this.Drones = new List<Drone>();
+        }
+
+        public int Count => this.Drones.Count;
+
+        public string AddDrone (Drone drone)
+        {
+            if (string.IsNullOrEmpty(drone.Name) || string.IsNullOrEmpty(drone.Brand) || drone.Range < 5 || drone.Range > 15)
+            {
+                return "Invalid drone";
+            }
+            
+            if(this.Drones.Count >= this.Capacity)
+            {
+                return "Airfield is full.";
+            }
+
+            Drones.Add(drone);
+            return $"Successfully added {drone.Name} to the airfield.";
+        }
+
+        public bool RemoveDrone(string name)
+        {
+            foreach (var drone in Drones)
+            {
+                if (drone.Name == name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int RemoveDroneByBrand(string brand)
+        {
+            int count = 0;
+
+            foreach (var drone in Drones)
+            {
+                if (drone.Brand == brand)
+                {
+                    count++;
+                }
+            }
+            Drones.RemoveAll(d => d.Brand == brand);
+            return count;
+        }
+
+        public Drone FlyDrone(string name)
+        {
+            foreach (var drone in Drones)
+            {
+                if (drone.Name == name)
+                {
+                    drone.Available = false;
+                    return drone;
+                }
+            }
+
+            return null;
+        }
+
+        public List<Drone> FlyDronesByRange(int range)
+        {
+            List<Drone> drones = new List<Drone>();
+
+            foreach (var drone in Drones)
+            {
+                if (drone.Range >= range)
+                {
+                    drone.Available = false;
+                    drones.Add(drone);
+                }
+            }
+
+            return drones;
+        }
+
+        public string Report()
+        {
+            var availableDrones = this.Drones.Where(d => d.Available == true);
+            return
+                $"Drones available at {this.Name}:" +
+                string.Join(Environment.NewLine, availableDrones);
+        }
+    }
+}
---
+++ C# Advanced/AdvancedModuleExamPrep/MealPlan/MealPlan.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MealPlan
+{
+
+    static void Main()
+    {
+        const int Salad = 350;
+        const int Soup = 490;
+        const int Pasta = 680;
+        const int Steak = 790;
+
+        string[] meals = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries); 
+        Queue<string> mealsQueue = new Queue<string>(meals);
+
+        int[] calories = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+        Stack<int> intakeCalories = new Stack<int>(calories);
+        int mealCounter = 0;
+
+        while (mealsQueue.Count > 0 && intakeCalories.Count > 0)
+        {
+            string checkMeal = mealsQueue.Dequeue();
+            int currMeal = 0;
+            if (checkMeal == "salad")
+            {
+                currMeal = Salad;
+            }
+            else if (checkMeal == "soup")
+            {
+                currMeal = Soup;
+            }
+            else if (checkMeal == "pasta")
+            {
+                currMeal = Pasta;
+            }
+            else if (checkMeal == "steak")
+            {
+                currMeal = Steak;
+            }
+
+            int availableCalories = intakeCalories.Pop();
+
+            
+            
+            if (availableCalories >= currMeal)
+            {
+                availableCalories -= currMeal;
+                mealCounter++;
+                if (availableCalories > 0)
+                {
+                    intakeCalories.Push(availableCalories);
+                }
+            }
+            else if (availableCalories < currMeal)
+            {
+                if (intakeCalories.Count == 0)
+                {
+                    intakeCalories.Clear();
+                    mealCounter++;
+                    break;
+                }
+                
+                availableCalories += intakeCalories.Pop();
+                availableCalories -= currMeal;
+                mealCounter++;
+                if (availableCalories > 0)
+                {
+                    intakeCalories.Push(availableCalories);
+                }
+            }
+
+        }
+
+        if (mealsQueue.Count == 0)
+        {
+            Console.WriteLine($"John had {mealCounter} meals.");
+            Console.WriteLine($"For the next few days, he can eat {string.Join(", ",intakeCalories)} calories.");
+        }
+        else if (intakeCalories.Count == 0)
+        {
+            Console.WriteLine($"John ate enough, he had {mealCounter} meals.");
+            Console.WriteLine($"Meals left: {string.Join(", ", mealsQueue)}.");
+        }
+    }
+}
